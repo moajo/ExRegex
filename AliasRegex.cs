535f42b4aa5f18b03d9a2c96397986ac2d984f82
moajo <mimirosiasd@gmail.com>
2016-09-05 11:52:51 +0900

    add Digit alias

---
+++ AliasRegex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExRegex.Match;
+
+namespace ExRegex
+{
+    /// <summary>
+    /// 簡略化表記
+    /// </summary>
+    public abstract class AliasRegex:Regex
+    {
+        public abstract Regex Content { get; }
+        public sealed override IEnumerable<RegexMatch> SimpleMatchings(StringPointer str, MatingContext context)
+        {
+            foreach (var match in Content.HeadMatches(str,context))
+            {
+                yield return new CompositeMatch(this,str,match);
+            }
+        }
+    }
+}
---
+++ Regexies/Aliases/digit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExRegex.Match;
+
+namespace ExRegex.Regexies.Aliases
+{
+    /// <summary>
+    /// \d
+    /// </summary>
+    public class Digit:AliasRegex
+    {
+        public override string Name
+        {
+            get { return "Digit"; }
+        }
+
+        public override Regex Content
+        {
+            get { return new Or("0","1", "2", "3", "4", "5", "6", "7", "8", "9"); }
+        }
+    }
+}
